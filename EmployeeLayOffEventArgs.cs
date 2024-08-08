29de1a41fdc18319611f87b16f31f05d4e9518ee
Akram Khattab <Akrammkhattab@gmail.com>
2024-08-09 00:37:25 +0300

    All Demo Has been updated

--- EmployeeLayOffEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace C42_G04_ADV04
-{
-    public class EmployeeLayOffEventArgs : EventArgs
-    {
-        public LayOffCause Cause { get; set; }
-    }
-}
---
+++ Fifa Game/Referee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_04_Advanced_C__G02_Demo.Fifa_Game
+{
+    internal class Refree
+    {
+        public string RefreeName { get; set; }
+        public void Look(Ball ball)
+        {
+            Console.WriteLine($"Refree: {RefreeName} is Looking At {ball}");
+        }
+
+
+    }
+}
